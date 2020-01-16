8f5f797875c046c46e3b20ebb910c70384d6fece
LUN7 <kwl188836@gmail.com>
2020-01-16 23:36:08 +0800

    update

--- arduinoCode/FYP.ino
+++ arduinoCode/FYP.ino
@@ -10,71 +10,83 @@ int RawValue = 0;
 int ACSoffset = 2500;
 float Voltage = 0;
 float Amps = 0;
-float humidity[5];
-float tempeature[7];
+float humidity[10];
+float tempeature[10];
 
-const int RELAY[6] = {48, 46, 44, 42, 40, 38}; //2V 4V 6V 8V 10V 12V 
-const int DHTPIN_0 = 45;
-const int DHTPIN_1 = 43;
-const int DHTPIN_2 = 41;
-const int DHTPIN_3 = 39;
-const int DHTPIN_4 = 37;
+class SensorsFeedBack {
+    public:
+        float humidity;
+        float tempeature;
+};
+
+SensorsFeedBack indoorAirData[4];
+SensorsFeedBack supplyAirData;
+
+const int RELAY[6] = { 48 , 46, 44 , 42 , 40 , 38 }; //2V 4V 6V 8V 10V 12V 
+const int DHTPIN[5] = { 45 , 43 , 41 , 39 , 37 };
+
+//const int DHTPIN_0 = 45;
+//const int DHTPIN_1 = 43;
+//const int DHTPIN_2 = 41;
+//const int DHTPIN_3 = 39;
+//const int DHTPIN_4 = 37;
+
 //fragiled sensor
+
+DHT dht[5] = {
+    DHT( DHTPIN[0] , DHTTYPE ) , 
+    DHT( DHTPIN[1] , DHTTYPE ) ,
+    DHT( DHTPIN[2] , DHTTYPE ) ,
+    DHT( DHTPIN[3] , DHTTYPE ) ,
+    DHT( DHTPIN[4] , DHTTYPE )
+    };
+
+//DHT dht0(DHTPIN_0, DHTTYPE)
+
 const int ampmeter = A0;
 const int chipSelect = 4 ;
 
 LiquidCrystal lcd(33, 31, 28, 26, 24, 22);
 
-DHT dht0(DHTPIN_0, DHTTYPE);
-DHT dht1(DHTPIN_1, DHTTYPE);
-DHT dht2(DHTPIN_2, DHTTYPE);
-DHT dht3(DHTPIN_3, DHTTYPE);
-DHT dht4(DHTPIN_4, DHTTYPE);
 
 void setup() {
 
-  Serial.begin(9600);
+    Serial.begin(9600);
 
-  lcd.begin(16, 2);
+    lcd.begin(16, 2);   
     PrintToLCD("Hello World");
     PrintToLCD("LUN FYP");
     PrintToLCD("Sys start up");
     
-  PrintToLCD("Init SD card");
-  Serial.println("Initialing SD card");
-  if (!SD.begin(chipSelect)) {
-    PrintToLCD("Card failed");
-    Serial.println("Card failed, or not present");
-    // don't do anything more:
-    while (1);
-  }
+    PrintToLCD("Init SD card");
+    Serial.println("Initialing SD card");
+    if (!SD.begin(chipSelect)) {
+        PrintToLCD("Card failed");
+        Serial.println("Card failed, or not present");
+        // don't do anything more:
+        while (1);
+    }
     PrintToLCD("Card init succ");
     Serial.println("card initialized.");
     delay(1000);
     PrintToLCD("Init sensors");
-  
-  dht0.begin();
-  dht1.begin();
-  dht2.begin();
-  dht3.begin();
-  dht4.begin();
-  pinMode(RELAY[0], OUTPUT);
-  pinMode(RELAY[1], OUTPUT);
-  pinMode(RELAY[2], OUTPUT);
-  pinMode(RELAY[3], OUTPUT);
-  pinMode(RELAY[4], OUTPUT);
-  pinMode(RELAY[5], OUTPUT);
 
-  digitalWrite(RELAY[0] , LOW); 
-  digitalWrite(RELAY[1] , LOW);
-  digitalWrite(RELAY[2] , LOW);
-  digitalWrite(RELAY[3] , LOW);
-  digitalWrite(RELAY[4] , LOW);
-  digitalWrite(RELAY[5] , LOW);
+    pinMode(RELAY[0], OUTPUT);
+    pinMode(RELAY[1], OUTPUT);
+    pinMode(RELAY[2], OUTPUT);
+    pinMode(RELAY[3], OUTPUT);
+    pinMode(RELAY[4], OUTPUT);
+    pinMode(RELAY[5], OUTPUT);
+    digitalWrite(RELAY[0] , LOW); 
+    digitalWrite(RELAY[1] , LOW);
+    digitalWrite(RELAY[2] , LOW);
+    digitalWrite(RELAY[3] , LOW);
+    digitalWrite(RELAY[4] , LOW);
+    digitalWrite(RELAY[5] , LOW);
 
-  PrintToLCD("sys start up");
-  PrintToLCD("successfully");
-}
+    PrintToLCD("sys start up");
+    PrintToLCD("successfully");
+}   
 
 void loop() {
     delay(1000);
@@ -89,7 +101,7 @@ void loop() {
 
 // function to print a device address
 void SerialPrintData(){
-    for (int i=0 ; i<=4 ; i++){
+    for (int i=0 ; i<5 ; i++){
         Serial.print(i);
         Serial.print("\t | Humidity: ");
         Serial.print(humidity[i]);
@@ -104,20 +116,15 @@ void SerialPrintData(){
 }
 
 void ReadTempAndHumidity(){
-    humidity[0] = dht0.readHumidity();
-    humidity[1] = dht1.readHumidity();
-    humidity[2] = dht2.readHumidity();
-    humidity[3] = dht3.readHumidity();
-    humidity[4] = dht4.readHumidity();
 
-    // Read temperature as Celsius
-    tempeature[0] = dht0.readTemperature();
-    tempeature[1] = dht1.readTemperature();
-    tempeature[2] = dht2.readTemperature();
-    tempeature[3] = dht3.readTemperature();
-    tempeature[4] = dht4.readTemperature();
-    //tempeature[6] = 
-    //tempeature[7] =  
+    supplyAirData.humidity = dht[0].readHumidity();
+    supplyAirData.tempeature = dht[0].readTemperature();
+    
+    for ( int i = 1 ; i < 5 ; i++ ){
+        indoorAirData[i].humidity = dht[i].readHumidity();
+        indoorAirData[i].tempeature = dht[i].readTemperature();
+    }
+    // Read temperature as Celsius   
 }
 
 void ReadAmp(){
@@ -146,10 +153,14 @@ void SaveData(){
     PrintToLCD("Data Storing...");
     // if the file is available, write to it:
     if (dataFile) {
-        for (int i = 0; i <= 4; i++){
-            dataFile.print(humidity[i]);
+        dataFile.print(supplyAirData.humidity);
+        dataFile.print(", ");
+        dataFile.print(supplyAirData.tempeature);
+        dataFile.print(", ");
+        for (int i = 1; i < 5 ; i++){
+            dataFile.print(indoorAirData[i].humidity);
             dataFile.print(", ");
-            dataFile.print(tempeature[i]);
+            dataFile.print(indoorAirData[i].tempeature);
             dataFile.print(", ");
         }
         dataFile.print(TECmode);
@@ -169,7 +180,7 @@ void SaveData(){
 
 
 int RandMode(){
-   return random(0,6);
+    return random(0,6);
 }
 
 void SetMode(){
